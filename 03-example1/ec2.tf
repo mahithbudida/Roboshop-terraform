fe505505693badb1882faca318236ff930bd46a2
KalluruS <>
2021-07-24 08:45:16 +0530

    Update terraform/

--- 03-example1/ec2.tf
+++ 03-example1/ec2.tf
@@ -23,7 +23,7 @@ resource "aws_security_group" "allow_ssh" {
   egress {
     from_port           = 0
     to_port             = 65535
-    protocol            = "tcp"
+    protocol            = "-1"
     cidr_blocks         = ["0.0.0.0/0"]
   }
 
