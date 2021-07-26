8ded1eeacb04a41b2bc16befbbd7942e733d029a
KalluruS <>
2021-07-26 09:07:52 +0530

    Update terraform/

--- 06-conditions/sample.tf
+++ 06-conditions/sample.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "sample" {
   //count                 = 0
   ami                   = "ami-074df373d6bafa625"
-  instance_type         = var.TYPE == "null" ? "t3.micro" : var.TYPE
+  instance_type         = var.TYPE == null ? "t3.micro" : var.TYPE
   //vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   tags                  = {
