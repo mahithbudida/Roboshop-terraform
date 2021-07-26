a1a6bbba1c62c5748eaaf263c6c01a83dc599054
KalluruS <>
2021-07-26 08:44:02 +0530

    Update terraform/

--- 05-modules/ec2/ec2.tf
+++ 05-modules/ec2/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "sample" {
+  count                 = 2
   ami                   = "ami-074df373d6bafa625"
-  instance_type         = "t3.micro"
+  instance_type         = var.TYPE
   vpc_security_group_ids = [var.SG_ID]
 
   tags                  = {
@@ -9,6 +10,7 @@ resource "aws_instance" "sample" {
 }
 
 variable "SG_ID" {}
+variable "TYPE" {}
 
 
 output "PRIVATE_IP" {
--- 05-modules/main.tf
+++ 05-modules/main.tf
@@ -7,8 +7,8 @@ module "ec2" {
   source  = "./ec2"
   SG_ID   = module.sg.SG_ID
   //AMI = "ami-074df373d6bafa625"
+  TYPE = "t3.small"
 }
-
 
 provider "aws" {
   region = "us-east-1"
