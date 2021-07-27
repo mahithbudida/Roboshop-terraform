46e7b8e81a0a09430d5f3ba0b82d83d5566f0cec
KalluruS <>
2021-07-27 08:34:03 +0530

    Update terraform/

--- roboshop-shell-scripting/ec2.tf
+++ roboshop-shell-scripting/ec2.tf
@@ -1,8 +1,9 @@
 resource "aws_spot_instance_request" "cheap_worker" {
-  count                     = length(var.COMPONENTS)
+  count                     = local.LENGTH
   ami                       = "ami-074df373d6bafa625"
   spot_price                = "0.0031"
   instance_type             = "t3.micro"
+  vpc_security_group_ids    = ["sg-0d00b6b80c9e9b60c"]
   wait_for_fulfillment      = true
 
   tags                      = {
@@ -11,9 +12,30 @@ resource "aws_spot_instance_request" "cheap_worker" {
 }
 
 resource "aws_ec2_tag" "name-tag" {
-  count                     = length(var.COMPONENTS)
+  count                     = local.LENGTH
   resource_id               = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
   key                       = "Name"
   value                     = element(var.COMPONENTS, count.index)
 }
 
+resource "null_resource" "run-shell-scripting" {
+  count                     = local.LENGTH
+  provisioner "remote-exec" {
+    connection {
+      host                  = element(aws_spot_instance_request.cheap_worker.*.public_ip, count.index)
+      user                  = "centos"
+      password              = "DevOps321"
+    }
+
+    command = [
+    "cd /home/centos",
+    "git clone https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps57/_git/shell-scripting",
+    "cd shell-scripting/roboshop",
+    "sudo make ${element(var.COMPONENTS, count.index)}"
+    ]
+  }
+}
+
+locals {
+  LENGTH    = length(var.COMPONENTS)
+}
