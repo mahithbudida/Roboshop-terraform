21145f3cca71cd0eb73733b61fd5dc4905ee838a
KalluruS <>
2021-07-27 08:21:52 +0530

    Update terraform/

--- roboshop-shell-scripting/ec2.tf
+++ roboshop-shell-scripting/ec2.tf
@@ -10,3 +10,13 @@ resource "aws_spot_instance_request" "cheap_worker" {
   }
 }
 
+//resource "aws_ec2_tag" "name-tag" {
+//  count                     = length(var.COMPONENTS)
+//  resource_id               = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
+//  key                       = "Name"
+//  value                     = element(var.COMPONENTS, count.index)
+//}
+
+output "attributes" {
+  value = aws_spot_instance_request.cheap_worker.*.spot_instance_id
+}
