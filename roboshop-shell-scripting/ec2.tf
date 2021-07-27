81759b248291285fbec4a214775d920a76a9a187
KalluruS <>
2021-07-27 08:45:10 +0530

    Update terraform/

--- roboshop-shell-scripting/ec2.tf
+++ roboshop-shell-scripting/ec2.tf
@@ -18,23 +18,30 @@ resource "aws_ec2_tag" "name-tag" {
   value                     = element(var.COMPONENTS, count.index)
 }
 
-resource "null_resource" "run-shell-scripting" {
+resource "aws_route53_record" "records" {
   count                     = local.LENGTH
-  provisioner "remote-exec" {
-    connection {
-      host                  = element(aws_spot_instance_request.cheap_worker.*.public_ip, count.index)
-      user                  = "centos"
-      password              = "DevOps321"
-    }
-
-    inline = [
-    "cd /home/centos",
-    "git clone https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps57/_git/shell-scripting",
-    "cd shell-scripting/roboshop",
-    "sudo make ${element(var.COMPONENTS, count.index)}"
-    ]
-  }
+  name                      = element(var.COMPONENTS, count.index)
+  type                      = "A"
+  zone_id                   = "Z00458692YJCZCCSC0W6X"
 }
+
+//resource "null_resource" "run-shell-scripting" {
+//  count                     = local.LENGTH
+//  provisioner "remote-exec" {
+//    connection {
+//      host                  = element(aws_spot_instance_request.cheap_worker.*.public_ip, count.index)
+//      user                  = "centos"
+//      password              = "DevOps321"
+//    }
+//
+//    inline = [
+//    "cd /home/centos",
+//    "git clone https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps57/_git/shell-scripting",
+//    "cd shell-scripting/roboshop",
+//    "sudo make ${element(var.COMPONENTS, count.index)}"
+//    ]
+//  }
+//}
 
 locals {
   LENGTH    = length(var.COMPONENTS)
