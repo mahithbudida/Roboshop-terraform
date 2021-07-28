41a27155cd42b638c863b5dfc52561108472e895
KalluruS <>
2021-07-28 07:43:30 +0530

    Update terraform/

--- 07-data-sources/data.tf
+++ 07-data-sources/data.tf
@@ -1,7 +1,6 @@
 data "aws_ami" "example" {
-  executable_users = ["self"]
   most_recent      = true
-  //name_regex       = "^myami-\\d{3}"
+  name_regex       = "^Centos*"
   owners           = ["973714476881"]
 }
 
