022ce033237cc344cda3d4274d8d4688305fd16c
mahithbudida <mahithbudida@outllook.com>
2021-08-03 23:03:10 +0530

    `Test file`

--- roboshop-shell-scripting/ec2.tf
+++ roboshop-shell-scripting/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_spot_instance_request" "cheap_worker" {
   ami                       = "ami-074df373d6bafa625"
   spot_price                = "0.0031"
   instance_type             = "t2.micro"
-  vpc_security_group_ids    = ["sg-0d00b6b80c9e9b60c"]
+  vpc_security_group_ids    = ["sg-061b8d4afe08bb944"]
   wait_for_fulfillment      = true
   //spot_type                 = "persistent"
   tags                      = {
